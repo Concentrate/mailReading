83abdd191efe52450efe0c135a54c613f3367049
liudeyu <qq1098014590@gmail.com>
2019-12-14 21:26:37 +0800

    让项目能整体跑起来

--- docs/sql/initsql.py
+++ docs/sql/initsql.py
@@ -5,6 +5,7 @@ import os.path
 if __name__=="__main__":
     cmdInit='mysql -u ldy -pabcd1234 < {0}'
     for tmp in os.listdir('.'):
-        rCmd=cmdInit.format(tmp)
-        print(rCmd)
-        os.popen(rCmd)
+        if tmp.endswith('sql'):
+            rCmd=cmdInit.format(tmp)
+            print(rCmd)
+            os.popen(rCmd)
